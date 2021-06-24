52485c8c275a8dc5f5e0afda7d53b931e0ef116e
Iurii Prytula <iurii.prytula@gmail.com>
2021-06-24 14:36:34 +0200

    app structure created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './app-material.module';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { TrainingComponent } from './components/training/training/training.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { CurrentTrainingComponent } from './components/training/current-training/current-training.component';
+import { NewTrainingComponent } from './components/training/new-training/new-training.component';
+import { PastTrainingsComponent } from './components/training/past-trainings/past-trainings.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingsComponent,
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
